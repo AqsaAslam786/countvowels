4d9d0ca76576ad26bd79f2e76c0ed8927db0c150
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-10-05 14:02:51 +0500

    save

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Triangleone from './components/Triangleone'
+
+function App() {
+  
+
+  return (
+    <>
+      <Triangleone />
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Triangleone.tsx
@@ -0,0 +1,30 @@
+
+const Triangleone = () => {
+//  const PrintTrianlge =(n:number): void => {
+//     for (let i = n; i > 0; i++){
+//         console.log(" *". repeat(i))
+
+//     }
+//  }
+ const takeAvg= (x: number, y: number): void => {
+    const avg =(x+y)/2;
+    console.log(avg);
+ }
+ const countZeros(...n.number{}): void{
+    let count = 0 
+for(let i=0; i< n.length; i++)
+    console.log([i])
+if (condition) {
+    
+}
+ }
+
+  return (
+    <div>
+      <>
+      <button className="bg-amber-600" onClick={() => takeAvg(5,2)}></button></>
+    </div>
+  )
+}
+
+export default Triangleone
